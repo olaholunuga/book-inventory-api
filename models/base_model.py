bc45bdf909551ddb68e5e59dd339d1aa7bd97e15
olaholunuga <olaholunuga@gmail.com>
2025-10-13 15:18:52 +0100

    created the BaseModel class with methods for initialization, string representation, saving data using the ORM

---
+++ models/base_model.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+"""basemodel for the books, authors and cartegories models
+"""
+from datetime import datetime
+from email.policy import default
+from sqlalchemy import Column, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+import uuid
+
+time = "%Y-%m-%dT%H:%M:%S.%f"
+Base = declarative_base()
+
+class BaseModel:
+    """the basemodel class from which the Books, Authors and Categories classes will be based
+    """
+    id = Column(String(60), primary_key=True)
+    created_at = Column(DateTime, default=datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.utcnow)
+
+    def __init__(self, *args, **kwargs) -> None:
+        """creating objects"""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    setattr(self, key, value)
+            if kwargs.get("created_at", None) and type(self.created_at) is str:
+                self.created_at = datetime.strptime(kwargs["created_at"], time)
+            else:
+                self.created_at = datetime.utcnow()
+            if kwargs.get("updated_at", None) and type(self.updated_at) is str:
+                self.updated_at = datetime.strptime(kwargs["updated_at"], time)
+            else:
+                self.updated_at = datetime.utcnow()
+            if kwargs.get("id", None) is None:
+                self.id = str(uuid.uuid4())
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = self.created_at
+
+    def __str__(self) -> str:
+        """String representation of the BaseModel and all derived classes"""
+        return "[{:s}] ({:s}) {}".format(
+            self.__class__.__name__,
+            self.id,
+            self.__dict__)
+
+    def save(self):
+        self.updated_at = datetime.utcnow()
+    
+    def to_dict(self, save_fs=None) -> dict:
+        """returns a dictionary containing all key/values of a model instance"""
+        new_dict = self.__dict__.copy()
+        if "created_at" in new_dict:
+            new_dict["created_at"] = new_dict["created_at"].strftime(time)
+        if "updated_at" in new_dict:
+            new_dict["updated_at"] = new_dict["updated_at"].strftime(time)
+        if "_sa_instance_state" in new_dict:
+            del new_dict["_sa_instance_state"]
+        if save_fs is None:
+            if "password" in new_dict:
+                del new_dict["password"]
+        return new_dict
+    
+    def delete(self):
+        pass
