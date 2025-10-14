607cb197ff181bd19a883db4e652f299f1ba8dd2
olaholunuga <olaholunuga@gmail.com>
2025-10-14 14:17:04 +0100

    created the author and category validation schemas

--- models/author.py
+++ models/author.py
@@ -2,6 +2,8 @@ from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String, Integer
 from models.book import book_authors
 from models.base_model import BaseModel, Base
+from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
+from marshmallow import fields, validates, ValidationError
 
 
 class Author(BaseModel, Base):
@@ -13,3 +15,17 @@ class Author(BaseModel, Base):
         secondary=book_authors,
         back_populates='authors'
     )
+
+class AuthorSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Author
+        load_instance = True
+        include_fk = True
+
+    name = fields.String(required=True)
+    books = fields.Nested("BookSchema", only=("id", "title"), many=True, dump_only=True)
+
+    @validates("name")
+    def validate_name(self, value):
+        if not value.strip():
+            raise ValidationError("Author name cannot be empty.")
--- models/category.py
+++ models/category.py
@@ -2,8 +2,10 @@ from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, Integer, String
 from models.book import book_categories
+from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
+from marshmallow import fields, validates, ValidationError
 
-class Category(Base):
+class Category(BaseModel, Base):
     __tablename__ = 'categories'
     name = Column(String)
 
@@ -12,3 +14,17 @@ class Category(Base):
         secondary=book_categories,
         back_populates='categories'
     )
+
+class CategorySchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Category
+        load_instance = True
+        include_fk = True
+
+    name = fields.String(required=True)
+    books = fields.Nested("BookSchema", only=("id", "title"), many=True, dump_only=True)
+
+    @validates("name")
+    def validate_name(self, value):
+        if not value.strip():
+            raise ValidationError("Category name cannot be empty.")
