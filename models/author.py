64b7d1d3b6cf21e7b312d7590ed40b635986a59f
olaholunuga <olaholunuga@gmail.com>
2025-10-20 14:23:40 +0100

    updated the author model to have a on-to-one relationship with the user model

--- models/author.py
+++ models/author.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import relationship
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, ForeignKey
 
 from models.base_model import BaseModel, Base, SoftDeleteMixin
 from models.book import book_authors
@@ -11,3 +11,13 @@ class Author(SoftDeleteMixin, BaseModel, Base):
     name = Column(String(128), nullable=False)  # not unique; validate non-empty in schema
 
     books = relationship("Book", secondary=book_authors, back_populates="authors")
+
+    user_id = Column(
+        String(128),
+        ForeignKey("users.id", ondelete="SET NULL"),
+        unique=True,
+        nullable=True,
+        index=True
+    )
+
+    user = relationship("User", back_populates="author")
