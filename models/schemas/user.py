3bba22032b89fc42e245e2baf06fc59fb6ef78b2
olaholunuga <olaholunuga@gmail.com>
2025-10-21 03:12:56 +0100

    auth

--- models/schemas/user.py
+++ models/schemas/user.py
@@ -1,4 +1,24 @@
 from marshmallow import Schema, fields
 
 class UserCreateSchema(Schema):
-    name = fields.String()
+    l_name = fields.String()
+    f_name = fields.String()
+    email = fields.String()
+    password = fields.String()
+
+class UserLoginSchema(Schema):
+    email = fields.String()
+    password = fields.String()
+
+class UserUpdateSchema(Schema):
+    f_name = fields.String(allow_none=True)
+    l_name = fields.String(allow_none=True)
+    email = fields.String(allow_none=True)
+    password = fields.String(allow_none=True)
+
+class UserOutSchema(Schema):
+    f_name = fields.String(allow_none=True)
+    l_name = fields.String(allow_none=True)
+    email = fields.String(allow_none=True)
+    admin = fields.Boolean()
+    literary_name = fields.String(allow_none=True)
